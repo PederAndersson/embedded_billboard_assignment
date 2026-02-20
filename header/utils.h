54524021f3eda4420c6e5be77f8a48868c6b9a50
Peder Andersson <peder.andersson@live.se>
2026-02-20 17:12:15 +0100

    fixed odd_even function so it now works as intended

--- header/utils.h
+++ header/utils.h
@@ -5,6 +5,7 @@
 #include "clientManager.h"
 
 
-void odd_even(uint32_t seconds, client *c);
+void odd_even(uint32_t seconds, client *c, uint8_t maxLen);
+void string_copy(char* strSrc, char* strDest, uint8_t maxLen);
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -2,16 +2,30 @@
 #include "utils.h"
 #include "lcd.h"
 
-void odd_even(uint32_t seconds, client *client){
+void odd_even(uint32_t seconds, client *client, uint8_t maxLen){
     lcd_set_cursor(0, 1);
+    char str[maxLen + 1];
     if (seconds%2){
-        for (uint8_t i = 0; i < ADD_LENGTH; i++){
-            lcd_write(*client->billboards[0][i]);
-        }
+        string_copy(client->billboards[0][0], str, maxLen);
     }else {
-        for (uint8_t i = 0; i < ADD_LENGTH; i++){ 
-            lcd_write(*client->billboards[1][i]);
-        }
+        string_copy(client->billboards[1][0], str, maxLen);
     }
+    for (uint8_t i = 0; i < maxLen; i++){
+        lcd_write(str[i]);
+    }
+}
+
+void string_copy(char* strSrc, char* strDest, uint8_t maxLen){
+    uint8_t i = 0;
+    
+    while (strSrc[i] != '\0' && i < maxLen){
+        strDest[i] = strSrc[i];
+        i++;
+    }
+    while(i < maxLen){
+        strDest[i] = ' ';
+        i++;
+    }
+    strDest[maxLen] = '\0';
 }
 
