ca32fad6a48b376aa7561aa2e9994ff06a05f87b
Peder Andersson <peder.andersson@live.se>
2026-02-21 11:17:00 +0100

    Due to changes in client struct deref client

--- src/utils.c
+++ src/utils.c
@@ -6,9 +6,9 @@ void odd_even(uint32_t seconds, client *client, uint8_t maxLen){
     lcd_set_cursor(0, 1);
     char str[maxLen + 1];
     if (seconds%2){
-        string_copy(client->billboards[0][0], str, maxLen);
+        string_copy(&client->billboards[0][0], str, maxLen);
     }else {
-        string_copy(client->billboards[1][0], str, maxLen);
+        string_copy(&client->billboards[1][0], str, maxLen);
     }
     for (uint8_t i = 0; i < maxLen; i++){
         lcd_write(str[i]);
